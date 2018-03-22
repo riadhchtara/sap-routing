62af4072cd9373520469e3ee8158dffa786d4c05
Riadh Chtara <riadh.chtara@sap.com>
2018-03-22 16:15:06 +0000

    delete control

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,44 +9,7 @@ sap.ui.define([
 		metadata: {
 			manifest: "json"
 		},
-		updateRoutes: function(conf) {
-			let routes = this.getRouter()._oRoutes;
 
-			for (let r in routes)
-				routes[r].destroy();
-			this.getRouter()._oRoutes = {};
-			let self = this;
-			conf.forEach(function(o) {
-				self.getRouter().addRoute({
-					"pattern": o.p + "W2",
-					"name": o.n,
-					"target": "default"
-				});
-				self.getRouter().getRoute(o.n).attachPatternMatched(o.callback, this);
-			});
-
-		},
-		addRoute: function(route) {
-
-			this.getRouter().addRoute({
-				"pattern": route.p,
-				"name": route.n,
-				"target": "default"
-			});
-			this.getRouter().getRoute(route.n).attachPatternMatched(route.callback, this);
-
-		},
-		getConfig: function() {
-			return [{
-				p: "detail/{invoicePath}",
-				n: "detail",
-				callback: this._detailMatched
-			}, {
-				p: "",
-				n: "default",
-				callback: this._defaultMatched
-			}]
-		},
 		init: function() {
 
 			// call the init function of the parent
@@ -55,44 +18,10 @@ sap.ui.define([
 			// create the views based on the url/hash
 
 			this.getRouter().initialize();
-			/*
-			let self = this;
-			var conf = this.getConfig();
 
-			conf.forEach(function(o) {
-				self.addRoute(o);
-
-			});*/
-			/*	conf.forEach(function(o) {
-				self.getRouter().addRoute({
-					"pattern": o.p,
-					"name": o.n,
-					"target": "default"
-				});
-				self.getRouter().getRoute(o.n).attachPatternMatched(o.callback, this);
-
-			});
-*/
-			//this.updateRoutes(conf);
-		/*	this.getRouter().parse(location.hash.substr(1));*/
-			
-			
-
-			//this._detailMatched();
-
-		},
-		_defaultMatched: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("page1");
-			console.log("default");
-		},
-		_detailMatched: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("page2");
-			console.log("detail" + oEvent.getParameter("arguments").invoicePath);
-		},
-		_bypassed: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("by");
 
 		}
+
 	});
 
 });
--- webapp/view/Default.view.js
+++ webapp/view/Default.view.js
@@ -4,76 +4,96 @@ sap.ui.jsview("sap.ui.demo.wt.view.Default", {
 	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
 	 * @memberOf view.View1
 	 */
-	getControllerName: function() {
-		return "sap.ui.demo.wt.controller.Default";
-	},
+
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	 * Since the Controller is given to this method, its event handlers can be attached right away. 
 	 * @memberOf view.View1
 	 */
-	
-		createContent: function(oController) {
 
-			
+	createContent: function(oController) {
 
-			
 		var oButton1 = new sap.m.Button(this.createId("navtomaster"), {
 			text: "navtomaster",
-			press: oController.navtomaster.bind(oController)
+			press: this.navtomaster.bind(this)
 		});
 		var oButton2 = new sap.m.Button(this.createId("navtodetail"), {
 			text: "navtodetail 1",
-			press: oController.navtodetail1.bind(oController)
+			press: this.navtodetail1.bind(this)
 		});
 		var oButton3 = new sap.m.Button(this.createId("navtodetail2"), {
 			text: "navtodetail 2",
-			press: oController.navtodetail2.bind(oController)
+			press: this.navtodetail2.bind(this)
 		});
-	
-		var page1=	new sap.m.Page("page1", {
+
+		var page1 = new sap.m.Page("page1", {
 			content: [
 				oButton1,
-				oButton2, 
+				oButton2,
 				oButton3
 			]
 		});
-		
-	var oButton4 = new sap.m.Button(this.createId("navtomaster4"), {
+
+		var oButton4 = new sap.m.Button(this.createId("navtomaster4"), {
 			text: "navtomaster",
-			press: oController.navtomaster.bind(oController)
+			press: this.navtomaster.bind(this)
 		});
 
-	
-
-	
-		var page2=	new sap.m.Page("page2", {
+		var page2 = new sap.m.Page("page2", {
 			content: [
 
-			oButton4
+				oButton4
 			]
 		});
-		var nav =new	sap.m.NavContainer("nav",{
+		var nav = new sap.m.NavContainer("nav", {
 			pages: [
 				page1, page2
 			]
 		});
 
-
-
 		return nav;
-		
-	},
-		_defaultMatched: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("page1");
-			console.log("default");
-		},
-		_detailMatched: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("page2");
-			console.log("detail" + oEvent.getParameter("arguments").invoicePath);
-		},
-		_bypassed: function(oEvent) {
-			sap.ui.getCore().byId("nav").to("by");
 
-		}
+	},
+	_defaultMatched: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("page1");
+		console.log("default");
+	},
+	_detailMatched: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("page2");
+		console.log("detail" + oEvent.getParameter("arguments").invoicePath);
+	},
+	_bypassed: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("by");
+
+	},
+
+	navtomaster: function() {
+
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("default"))
+			oRouter.navTo("default");
+
+		// show message
+
+	},
+
+	navtodetail1: function() {
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("detail"))
+			oRouter.navTo("detail", {
+				invoicePath: "1"
+			});
+
+		// show message
+
+	},
+	navtodetail2: function() {
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("detail"))
+			oRouter.navTo("detail", {
+				invoicePath: "2"
+			});
+		// show message
+
+	},
 });
