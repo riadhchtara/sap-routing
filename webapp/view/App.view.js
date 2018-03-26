c7e9f2a382bc51ea7dc878bd6a3b862adf0539e0
Riadh Chtara <riadh.chtara@sap.com>
2018-03-26 15:01:05 +0000

    before renaming

--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -1,15 +1,5 @@
 sap.ui.jsview("sap.ui.demo.wt.view.App", {
 
-	/** Specifies the Controller belonging to this View. 
-	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	 * @memberOf view.View1
-	 */
-
-	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	 * Since the Controller is given to this method, its event handlers can be attached right away. 
-	 * @memberOf view.View1
-	 */
-
 	addRoute: function(route) {
 		let router = sap.ui.core.UIComponent.getRouterFor(this);
 
@@ -56,107 +46,10 @@ sap.ui.jsview("sap.ui.demo.wt.view.App", {
 		sap.ui.getCore().byId("nav").to("page2");
 		console.log("detail" + oEvent.getParameter("arguments").invoicePath);
 	},
-	_bypassed: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("by");
-
-	},
 	createContent: function(oController) {
 	    setTimeout(function(){	this.initRouting();}.bind(this), 5000);
 		return new sap.m.App({
 			id: "app"
 		});
-	}
-	/*
-	createContent: function(oController) {
-
-		var oButton1 = new sap.m.Button(this.createId("navtomaster"), {
-			text: "navtomaster",
-			press: this.navtomaster.bind(this)
-		});
-		var oButton2 = new sap.m.Button(this.createId("navtodetail"), {
-			text: "navtodetail 1",
-			press: this.navtodetail1.bind(this)
-		});
-		var oButton3 = new sap.m.Button(this.createId("navtodetail2"), {
-			text: "navtodetail 2",
-			press: this.navtodetail2.bind(this)
-		});
-
-		var page1 = new sap.m.Page("page4", {
-			content: [
-				oButton1,
-				oButton2,
-				oButton3
-			]
-		});
-
-		var oButton4 = new sap.m.Button(this.createId("navtomaster4"), {
-			text: "navtomaster",
-			press: this.navtomaster.bind(this)
-		});
-
-		var page2 = new sap.m.Page("page2", {
-			content: [
-
-				oButton4
-			]
-		});
-		var nav = new sap.m.NavContainer("nav", {
-			pages: [
-				page1, page2
-			]
-		});
-		
-			    setTimeout(function(){	this.initRouting();}.bind(this), 5000);
-		let n =  new sap.m.App({
-			id: "app",
-			content : nav
-		});
-
-		return n;
-
-	}
-	*/,
-	_defaultMatched: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("page1");
-		console.log("default");
-	},
-	_detailMatched: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("page2");
-		console.log("detail" + oEvent.getParameter("arguments").invoicePath);
-	},
-	_bypassed: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("by");
-
-	},
-
-	navtomaster: function() {
-
-		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		if (oRouter.getRoute("default"))
-			oRouter.navTo("default");
-
-		// show message
-
-	},
-
-	navtodetail1: function() {
-		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		if (oRouter.getRoute("detail"))
-			oRouter.navTo("detail", {
-				invoicePath: "1"
-			});
-
-		// show message
-
-	},
-	navtodetail2: function() {
-		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		if (oRouter.getRoute("detail"))
-			oRouter.navTo("detail", {
-				invoicePath: "2"
-			});
-		// show message
-
 	}
 });
--- webapp/view/Default.view.js
+++ webapp/view/Default.view.js
@@ -1,16 +1,5 @@
 sap.ui.jsview("sap.ui.demo.wt.view.Default", {
 
-	/** Specifies the Controller belonging to this View. 
-	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	 * @memberOf view.View1
-	 */
-
-
-	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	 * Since the Controller is given to this method, its event handlers can be attached right away. 
-	 * @memberOf view.View1
-	 */
-
 	createContent: function(oController) {
 
 		var oButton1 = new sap.m.Button(this.createId("navtomaster"), {
@@ -52,18 +41,6 @@ sap.ui.jsview("sap.ui.demo.wt.view.Default", {
 		});
 
 		return nav;
-
-	},
-	_defaultMatched: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("page1");
-		console.log("default");
-	},
-	_detailMatched: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("page2");
-		console.log("detail" + oEvent.getParameter("arguments").invoicePath);
-	},
-	_bypassed: function(oEvent) {
-		sap.ui.getCore().byId("nav").to("by");
 
 	},
 
