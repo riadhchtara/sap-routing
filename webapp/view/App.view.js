9148d8b99b298184cf55e9214759651c20dba319
Riadh Chtara <riadh.chtara@sap.com>
2018-03-26 14:16:51 +0000

    fix carlo

--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -66,4 +66,97 @@ sap.ui.jsview("sap.ui.demo.wt.view.App", {
 			id: "app"
 		});
 	}
+	/*
+	createContent: function(oController) {
+
+		var oButton1 = new sap.m.Button(this.createId("navtomaster"), {
+			text: "navtomaster",
+			press: this.navtomaster.bind(this)
+		});
+		var oButton2 = new sap.m.Button(this.createId("navtodetail"), {
+			text: "navtodetail 1",
+			press: this.navtodetail1.bind(this)
+		});
+		var oButton3 = new sap.m.Button(this.createId("navtodetail2"), {
+			text: "navtodetail 2",
+			press: this.navtodetail2.bind(this)
+		});
+
+		var page1 = new sap.m.Page("page4", {
+			content: [
+				oButton1,
+				oButton2,
+				oButton3
+			]
+		});
+
+		var oButton4 = new sap.m.Button(this.createId("navtomaster4"), {
+			text: "navtomaster",
+			press: this.navtomaster.bind(this)
+		});
+
+		var page2 = new sap.m.Page("page2", {
+			content: [
+
+				oButton4
+			]
+		});
+		var nav = new sap.m.NavContainer("nav", {
+			pages: [
+				page1, page2
+			]
+		});
+		
+			    setTimeout(function(){	this.initRouting();}.bind(this), 5000);
+		let n =  new sap.m.App({
+			id: "app",
+			content : nav
+		});
+
+		return n;
+
+	}
+	*/,
+	_defaultMatched: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("page1");
+		console.log("default");
+	},
+	_detailMatched: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("page2");
+		console.log("detail" + oEvent.getParameter("arguments").invoicePath);
+	},
+	_bypassed: function(oEvent) {
+		sap.ui.getCore().byId("nav").to("by");
+
+	},
+
+	navtomaster: function() {
+
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("default"))
+			oRouter.navTo("default");
+
+		// show message
+
+	},
+
+	navtodetail1: function() {
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("detail"))
+			oRouter.navTo("detail", {
+				invoicePath: "1"
+			});
+
+		// show message
+
+	},
+	navtodetail2: function() {
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		if (oRouter.getRoute("detail"))
+			oRouter.navTo("detail", {
+				invoicePath: "2"
+			});
+		// show message
+
+	}
 });
