2fb94d0a28bb932c4260fb78f8eb2e3cf2038cc0
Riadh Chtara <riadh.chtara@sap.com>
2018-04-13 10:25:57 +0000

    split

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,44 +1,48 @@
 sap.ui.jsview("routerApp.view.App", {
 
-  initRouting: function () {
-    let router = sap.ui.core.UIComponent.getRouterFor(this);
-    router.initialize();
+	initRouting: function() {
+		let router = sap.ui.core.UIComponent.getRouterFor(this);
+		router.initialize();
 
-  },
-  createContent: function (oController) {
-    var r = new sap.m.App({
-      id: "apppp"
-    });
+	},
+	createContent: function(oController) {
+		var r = new sap.m.App({
+			id: "apppp"
+		});
 
-    this.initRouting();
-    return r;
-  }
+		this.initRouting();
+		return r;
+	}
 });
 
 sap.ui.jsview("routerApp.view.Default", {
 
-  createContent: function (oController) {
-    let router = sap.ui.core.UIComponent.getRouterFor(this);
-    let r = HealthApp.init('this.getManifestEntry("/sap.app/id")', router);
+	createContent: function(oController) {
+		let router = sap.ui.core.UIComponent.getRouterFor(this);
+		let r = HealthApp.init('this.getManifestEntry("/sap.app/id")', router);
 
-    HealthApp.inited();
-    return r;
-  }
+	//	HealthApp.inited();
+		return r;
+	}
 });
 
 sap.ui.define([
-  "sap/ui/core/UIComponent",
-  "sap/ui/model/json/JSONModel"
-], function (UIComponent, JSONModel) {
-  "use strict";
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function(UIComponent, JSONModel) {
+	"use strict";
 
-  return UIComponent.extend("routerApp.Component", {
+	return UIComponent.extend("routerApp.Component", {
 
-    metadata: {
-      manifest: "json"
-    },
-    init: function () {
-      UIComponent.prototype.init.apply(this, arguments);
-    }
-  });
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			if (typeof stand === "undefined"){
+			
+					
+			}
+			UIComponent.prototype.init.apply(this, arguments);
+		}
+	});
 });
